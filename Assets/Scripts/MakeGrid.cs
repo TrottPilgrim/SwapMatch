91bb3d25e6b62b3a3d0ad3187bd56aa0a5f68443
Thomas Lim <tl1860@nyu.edu>
2019-03-26 19:50:24 -0400

    First commit!

---
+++ Assets/Scripts/MakeGrid.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeGrid : MonoBehaviour
+{
+    public const int WIDTH = 5;
+    public const int HEIGHT = 7;
+
+    float xOffset = WIDTH / 2f - 0.5f;
+    float yOffset = HEIGHT / 2f - 0.5f;
+
+    public GameObject[,] tiles;
+    public GameObject tilePrefab;
+
+    GameObject gridHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiles = new GameObject[WIDTH, HEIGHT];
+        gridHolder = new GameObject();
+        gridHolder.transform.position = new Vector3(-1f, -0.5f, 0);
+        
+        for (int i = 0; i < WIDTH; i++){
+            for (int j = 0; j < HEIGHT; j++){
+                GameObject newTile = Instantiate(tilePrefab);
+                newTile.transform.parent = gridHolder.transform;
+                newTile.transform.localPosition = new Vector2(WIDTH - i - xOffset, HEIGHT - j - yOffset);
+
+                tiles[i, j] = newTile;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
