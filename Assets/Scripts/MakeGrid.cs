464899d11a1fc5467e2946a73f7980c9bde6f70a
Thomas Lim <tl1860@nyu.edu>
2019-03-28 20:05:35 -0400

    update

--- Assets/Scripts/MakeGrid.cs
+++ Assets/Scripts/MakeGrid.cs
@@ -13,7 +13,10 @@ public class MakeGrid : MonoBehaviour
     public GameObject[,] tiles;
     public GameObject tilePrefab;
 
+    public GameObject playerFab;
+
     GameObject gridHolder;
+    PlayerScript playerScript;
 
     // Start is called before the first frame update
     void Start()
@@ -35,11 +38,32 @@ public class MakeGrid : MonoBehaviour
                 tileScript.SetSprite(Random.Range(0, tileScript.tileColors.Length));
             }
         }
+        GameObject player = Instantiate(playerFab);
+        playerScript = player.GetComponent<PlayerScript>();
+        player.transform.parent = gridHolder.transform;
+        Vector2 playerStartPos = tiles[2, 3].transform.localPosition;
+        Destroy(tiles[2, 3]);
+        player.transform.localPosition = playerStartPos;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxis("Horizontal") > 0){
+            
+            if (playerScript.xPos <= HEIGHT && playerScript.xPos >= 0){
+                GameObject tileToSwap = tiles[playerScript.xPos + 1, playerScript.yPos];
+            }
+        }
+        else if (Input.GetAxis("Horizontal") < 0) {
+
+        }
+        if (Input.GetAxis("Vertical") > 0){
+
+        }
+        else if (Input.GetAxis("Vertical") < 0) {
+
+        }
     }
 }
