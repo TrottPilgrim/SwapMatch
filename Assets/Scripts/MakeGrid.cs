f889cfee591a3d9950d5ea215cae40cd425f2fbe
Thomas Lim <tl1860@nyu.edu>
2019-04-09 15:06:05 -0400

    cleaned up movement code

--- Assets/Scripts/MakeGrid.cs
+++ Assets/Scripts/MakeGrid.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MakeGrid : MonoBehaviour
 {
+    public static MakeGrid Instance;
     public const int WIDTH = 5;
     public const int HEIGHT = 7;
 
@@ -21,6 +22,7 @@ public class MakeGrid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Instance = this;
         tiles = new GameObject[WIDTH, HEIGHT];
         gridHolder = new GameObject();
         gridHolder.transform.position = new Vector3(-1f, -0.5f, 0);
@@ -28,11 +30,12 @@ public class MakeGrid : MonoBehaviour
         for (int i = 0; i < WIDTH; i++){
             for (int j = 0; j < HEIGHT; j++){
                 GameObject newTile = Instantiate(tilePrefab);
-                newTile.name = i + "," + j;
+                //newTile.name = i + "," + j;
                 newTile.transform.parent = gridHolder.transform;
                 //WHY does this matrix go from upper right to bottom left. Is this supposed to happen?
-                //newTile.transform.localPosition = new Vector2(WIDTH - i - xOffset, HEIGHT - j - yOffset);
-                newTile.transform.localPosition = new Vector2( i - 1, HEIGHT - j - yOffset);
+                //I know why, it's because then you can just use Input.GetAxis for movement.
+                newTile.transform.localPosition = new Vector2(WIDTH - i - xOffset, HEIGHT - j - yOffset);
+                //newTile.transform.localPosition = new Vector2( i - 1, HEIGHT - j - yOffset);
 
                 tiles[i, j] = newTile;
                 TileScript tileScript = newTile.GetComponent<TileScript>();
@@ -47,86 +50,10 @@ public class MakeGrid : MonoBehaviour
         Vector2 playerStartPos = tiles[playerInitXPos, playerInitYPos].transform.localPosition;
         Destroy(tiles[playerInitXPos, playerInitYPos]);
         player.transform.localPosition = playerStartPos;
+        tiles[playerInitXPos, playerInitYPos] = player;
         playerScript.xPos = playerInitXPos;
         playerScript.yPos = playerInitYPos; 
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("d") || Input.GetKeyDown(KeyCode.RightArrow)){
-            if (playerScript.xPos < WIDTH - 1){
-                GameObject tileToSwap = tiles[playerScript.xPos + 1, playerScript.yPos];
-                Vector2 newPosition = tileToSwap.transform.localPosition;
-
-                // Changes the positions of the two objects in the game space
-                tileToSwap.transform.localPosition = playerScript.gameObject.transform.localPosition;
-                playerScript.gameObject.transform.localPosition = newPosition;
-
-                // Changes the positions of the two objects in the 2D array
-                tiles[playerScript.xPos + 1, playerScript.yPos] = playerScript.gameObject;
-                tiles[playerScript.xPos, playerScript.yPos] = tileToSwap;
-                playerScript.xPos++;
-                playerScript.gameObject.SendMessage("decrementTurns");
-            }
-        }
-        else if (Input.GetKeyDown("a") || Input.GetKeyDown(KeyCode.LeftArrow)){
-            if (playerScript.xPos > 0){
-                GameObject tileToSwap = tiles[playerScript.xPos - 1, playerScript.yPos];
-                Vector2 newPosition = tileToSwap.transform.localPosition;
-                
-                // Changes the positions of the two objects in the game space
-                tileToSwap.transform.localPosition = playerScript.gameObject.transform.localPosition;
-                playerScript.gameObject.transform.localPosition = newPosition;
-
-                // Changes the positions of the two objects in the 2D array
-                tiles[playerScript.xPos - 1, playerScript.yPos] = playerScript.gameObject;
-                tiles[playerScript.xPos, playerScript.yPos] = tileToSwap;
-                playerScript.xPos--;
-                playerScript.gameObject.SendMessage("decrementTurns");
-            }
-        }
-        
-        else if (Input.GetKeyDown("w") || Input.GetKeyDown(KeyCode.UpArrow)){
-            if (playerScript.yPos > 0){
-                GameObject tileToSwap = tiles[playerScript.xPos, playerScript.yPos - 1];
-                Vector2 newPosition = tileToSwap.transform.localPosition;
-                
-                // Changes the positions of the two objects in the game space
-                tileToSwap.transform.localPosition = playerScript.gameObject.transform.localPosition;
-                playerScript.gameObject.transform.localPosition = newPosition;
-
-                // Changes the positions of the two objects in the 2D array
-                tiles[playerScript.xPos, playerScript.yPos - 1] = playerScript.gameObject;
-                tiles[playerScript.xPos, playerScript.yPos] = tileToSwap;
-                playerScript.yPos--;
-                playerScript.gameObject.SendMessage("decrementTurns");
-            }
-        }
-        else if (Input.GetKeyDown("s") || Input.GetKeyDown(KeyCode.DownArrow)){
-            if (playerScript.yPos < HEIGHT - 1){
-                GameObject tileToSwap = tiles[playerScript.xPos, playerScript.yPos + 1];
-                Vector2 newPosition = tileToSwap.transform.localPosition;
-                
-                // Changes the positions of the two objects in the game space
-                tileToSwap.transform.localPosition = playerScript.gameObject.transform.localPosition;
-                playerScript.gameObject.transform.localPosition = newPosition;
-
-                // Changes the positions of the two objects in the 2D array
-                tiles[playerScript.xPos, playerScript.yPos + 1] = playerScript.gameObject;
-                tiles[playerScript.xPos, playerScript.yPos] = tileToSwap;
-                playerScript.yPos++;
-                playerScript.gameObject.SendMessage("decrementTurns");
-            }
-        }
-    }
-
-    /*
-        This function checks if a line has some matching sprites
-        Inputs: initial X position, initial Y position 
-     */
-    void checkLine() {
-    
-    }
 }
--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -10,11 +10,46 @@ public class MoveScript : MonoBehaviour
     void Start()
     {
         playerScript = gameObject.GetComponentInParent(typeof(PlayerScript)) as PlayerScript;
+        tiles = MakeGrid.Instance.tiles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown){
+            MovePlayer();
+        }
+    }
+
+    /* 
+     */
+    void MovePlayer(){
+        int hozMove = (int) Input.GetAxisRaw("Horizontal");
+        int verMove = (int) Input.GetAxisRaw("Vertical");
+        if (hozMove != 0)
+            verMove = 0;
+        else if (verMove != 0)
+            hozMove = 0;
+
+        //Get the tile that needs to swap with the player and save its position
+        GameObject tileToSwap = tiles[playerScript.xPos - hozMove, playerScript.yPos - verMove];
+        Vector2 newPosition = tileToSwap.transform.localPosition;
+    
+        //Swap the locations of the two objects in the game space
+        tileToSwap.transform.localPosition = playerScript.gameObject.transform.localPosition;
+        playerScript.gameObject.transform.localPosition = newPosition;
+
+        //Swap the two objects in the 2D array
+        tiles[playerScript.xPos - hozMove, playerScript.yPos - verMove] = playerScript.gameObject;
+        tiles[playerScript.xPos, playerScript.yPos] = tileToSwap;
+
+        //Update the x position and y position of the player.
+        playerScript.xPos -= hozMove;
+        playerScript.yPos -= verMove;
+        // Debug.Log(playerScript.xPos + " " + playerScript.yPos);
+        // Debug.Log(tiles[0, 0]);
+        playerScript.gameObject.SendMessage("decrementTurns");
+
+
     }
 }
